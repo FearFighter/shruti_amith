029d987715488c6882caef7df3395bfa807f8984
amith prasad <amithprasad@amiths-MacBook-Pro-13.local>
2019-10-12 12:19:06 +0530

    fixed stuff

--- dist/scripts.js
+++ dist/scripts.js
@@ -856,7 +856,7 @@ $(document).ready(function() {
 	};
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Feb 06, 2020 11:00:00").getTime();
+		var countDownDate = new Date("Feb 06, 2020 19:00:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
